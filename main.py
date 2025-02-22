5204fa38135c98531ba79003d6efcce36c374b91
czolowek <klutoi39@gmail.com>
2025-02-22 21:38:49 +0100

    пвывпвпав

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from forms import SignUpForm, LoginForm
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,3 +36,57 @@ user_tours = db.Table('user_tours',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
     db.Column('tour_id', db.Integer, db.ForeignKey('tour.id'), primary_key=True)
 )
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+    tours = Tour.query.all()
+    return render_template('index.html', tours=tours)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user = User(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!', 'success')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.is_validate_password(form.password.data):
+            login_user(user)
+            flash('Вы успешно вошли в систему!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Неправильный email или пароль', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы вышли из системы', 'info')
+    return redirect(url_for('index'))
+
+@app.route('/cabinet')
+@login_required
+def cabinet():
+    return render_template('cabinet.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Новая папка/data/forms.py
+++ Новая папка/data/forms.py
@@ -1,104 +1,31 @@
-from flask_wtf import FlaskForm
-import wtforms
-
-
-
-
-class SignUpform(FlaskForm):
-    first_name =  wtforms.StringField("введить свой имя")
-    last_name =  wtforms.StringField("введить свой фамилия")
-    email = wtforms.EmailField("Email", validators=[wtforms.validators.Email(), wtforms.validators.data_required()])
-    password = wtforms.PasswordField("введит парол",validators=[wtforms.validators.length(8)])
-    submit = wtforms.SubmitField("зарегистрироваться")
-
-
-class Loginform(FlaskForm):
-    email = wtforms.EmailField("Email", validators=[wtforms.validators.Email(), wtforms.validators.data_required()])
-    password = wtforms.PasswordField("введит парол",validators=[wtforms.validators.length(8)])
-    submit = wtforms.SubmitField("войти")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    tours = db.relationship('Tour', secondary='user_tours', backref='users')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def is_validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Tour(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    departure = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+user_tours = db.Table('user_tours',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('tour_id', db.Integer, db.ForeignKey('tour.id'), primary_key=True)
+)
